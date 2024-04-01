93d6d8eff8335db82574e8d686b902d8abfb30a0
CreativeGold <164043625+CreativeGold@users.noreply.github.com>
2024-04-01 18:02:27 -0400

    added exploring logical operations

--- Exploring_Logical_Operations_And_Precedence.py
+++ Exploring_Logical_Operations_And_Precedence.py
@@ -13,3 +13,13 @@ else:
     print("The results do not match:")
     print("Result without parentheses:", result_no_parentheses)
     print("Result with parentheses:", result_with_parentheses)
+
+
+
+# Task 2: Mix and Match
+    
+# Task Expression
+outcome = (7 + 3) > 5 or (2 * 4) == 7
+
+# Printed Outcome
+print("Outcome:", outcome)
