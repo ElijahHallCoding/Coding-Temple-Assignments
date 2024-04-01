e68bd06117c3983f35f66c7d45d2cabfa2217722
CreativeGold <164043625+CreativeGold@users.noreply.github.com>
2024-04-01 14:18:42 -0400

    main

---
+++ Python_Data_Types_And_Type_Function.py
@@ -0,0 +1,7 @@
+# 3 Python Data Types and type() Function
+
+# Variables
+variable_a, variable_b, variable_c, variable_d = "Hello, World", 23, 3.14, True
+
+# Print variable type
+print(type(variable_a), type(variable_b), type(variable_c), type(variable_d))
