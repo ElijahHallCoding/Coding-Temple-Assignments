58bb732a5645c1b15002f66f924caf02eb7b3041
CreativeGold <164043625+CreativeGold@users.noreply.github.com>
2024-04-01 17:29:08 -0400

    main

---
+++ Understanding_Assignments_And_Comparisons.py
@@ -0,0 +1,14 @@
+# Task 1: Value Swapping
+
+# Chosen numbers
+number1 = 10
+number2 = 20
+
+# Value swap using assignment operators
+number1 = number1 + number2
+number2 = number1 - number2
+number1 = number1 - number2
+
+# Values after swap
+print("Number 1 after swap:", number1)
+print("Number 2 after swap:", number2)
