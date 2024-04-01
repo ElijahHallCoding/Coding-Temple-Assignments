9e71d3bd394ec2a5e5e174ca3f35bdca5ccf7bcc
CreativeGold <164043625+CreativeGold@users.noreply.github.com>
2024-04-01 13:50:07 -0400

    main

---
+++ Python_Naming_Convention_Practice.py
@@ -0,0 +1,5 @@
+def add(num1: int, num2: int) -> int:
+    """Add two numbers"""
+    num3 = num1 + num2
+ 
+    return num3
---
+++ Showcase_Your_Dance_Moves.py
@@ -0,0 +1,21 @@
+# Task 1: Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella") 
+
+# Task 2: Your Mood Today
+    
+# User mood input
+user_mood = input("How do you feel today? ")
+
+# Check the user's mood and respond accordingly
+if user_mood.lower() == "happy":
+    print("That's great to hear!")
+elif user_mood.lower() == "sad":
+    print("I hope your day gets better!")
+else:
+    print("Sweet! Enjoy the rest of your day!")
