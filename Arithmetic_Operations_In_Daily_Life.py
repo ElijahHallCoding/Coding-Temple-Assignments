e8c0662f1a709f22ccb17735b69656b947088f9c
CreativeGold <164043625+CreativeGold@users.noreply.github.com>
2024-04-01 17:13:08 -0400

    main

---
+++ Arithmetic_Operations_In_Daily_Life.py
@@ -0,0 +1,24 @@
+# Task 1: Grocery Store Math
+
+bread = 6.99
+peanut_butter = 4.59
+jelly = 5.30
+total_cost = bread + peanut_butter + jelly
+print(total_cost)
+
+# Task 2: Bank Interest
+
+# Savings account initial balance
+initial_amount = 10000  # $10,000
+
+# Yearly interest rate 
+yearly_interest_rate = 7  # 7%
+
+# Convert the percentage interest rate to a decimal
+interest_rate_decimal = yearly_interest_rate / 100
+
+# Calculate the total amount after one year
+total_amount = initial_amount * (6 + interest_rate_decimal)
+
+# Total after 1 year
+print("Total amount after one year:", total_amount)
